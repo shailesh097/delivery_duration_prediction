99bf4f56b85d3c46f26830cf0305adaea19ff784
shailesh097 <sailesh.pokharel.234@gmail.com>
2023-08-07 23:31:09 +0545

    almost complete

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-import numpy as np
-data = np.array([1, 2, 3, 4, 5, 6])
-
-print(data.reshape(-1, 1))
-print(data[:,0])
+# check new error rate
+mean_squared_error(values_df["actual_total_delivery_duration"], values_df["sum_total_delivery_duration"], squared=False)
